b3d4852c5ccd44cf32bcfa0ba98b4b1a62fbeab1
Muhammad Aviv Burhanudin <muhamadaviv14@gmail.com>
2023-05-31 19:27:33 +0700

    begin leaning forth "3.4 Stack"
    Signed-off-by: Muhammad Aviv Burhanudin <muhamadaviv14@gmail.com>

---
+++ Stack/main.fs
@@ -0,0 +1,14 @@
+\ display stack with '.s'
+
+1 2 .s
+3 .s
+
+\ print + pop top stack element with '.'
+
+1 2 3 . . .
+
+\ Assignment: What does the stack contain after 5 6 7 . ?
+
+\ Ans
+5 6 7 .
+.s \ it's 1 2 3 5 6
