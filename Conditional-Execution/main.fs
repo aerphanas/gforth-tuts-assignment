5e0ada1572ffe290c4e89e41f2d82359985b421f
Muhammad Aviv Burhanudin <muhamadaviv14@gmail.com>
2023-06-01 17:56:24 +0700

    forth tutorial 3.16 Conditional execution
    Signed-off-by: Muhammad Aviv Burhanudin <muhamadaviv14@gmail.com>

---
+++ Conditional-Execution/main.fs
@@ -0,0 +1,38 @@
+\ In Forth you can use control structures only inside colon definitions.
+
+: abs ( n1 -- +n2 )
+  dup  0 < IF
+    negate
+    endif ;
+
+\ if takes a flag from the stack. If the flag is non-zero (true),
+\ the following code is performed, otherwise execution continues after the endif (or else).
+
+\  < compares the top two stack elements and produces a flag
+
+1 2 < .
+2 1 < .
+1 1 < .
+
+\ non zero is true
+\ zero is false
+
+\ Actually the standard name for endif is then.
+
+\ You can optionally use an else-part
+
+: min ( n1 n2 -- n )
+  2dup < if
+    drop
+  else
+    nip
+  then ;
+
+\ Assignment: Write min without else-part (hint: what's the definition of nip?). 
+\ TODO: update
+: min ( n1 n2 -- n )
+  2dup < if
+    drop
+  else
+    swap drop
+  then ;
